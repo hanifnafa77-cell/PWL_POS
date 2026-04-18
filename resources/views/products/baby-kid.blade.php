84fd92c60d816214553bbfcfe9d0c09509e9f4c0
Nurulfaizah-18 <sayafaizah18@gmail.com>
2026-04-18 13:55:15 +0700

    pos laravel project

---
+++ resources/views/products/baby-kid.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kategori Produk</title>
+</head>
+<body>
+    <h1>Daftar Produk: Baby & Kid</h1>
+    <p>Ini adalah halaman yang menampilkan produk perlengkapan bayi dan anak-anak.</p>
+</body>
+</html>
