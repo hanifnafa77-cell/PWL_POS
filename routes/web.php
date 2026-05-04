cb497a18c607926c17e5aa6b0f9d5f9ccf39fbc1
wikkyardiansyah <wikkyardiansyah79@gmail.com>
2026-05-04 13:31:27 +0700

    pwlpos

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,15 +12,21 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Halaman Utama / Dashboard
+Route::get('/', [WelcomeController::class, 'index']);
+
+// Kelompok rute untuk User menggunakan Yajra Datatables
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);          // Menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);      // Memanggil fungsi list untuk ajax datatables
+    Route::get('/create', [UserController::class, 'create']);   // Menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);         // Menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);       // Menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);   // Menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);     // Menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); // Menghapus data user
 });
 
+// Rute lainnya (Level & Kategori)
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
